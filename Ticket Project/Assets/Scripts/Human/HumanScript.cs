29a35f2b9555400a3533190bad4b3cab73e8637c
mamanju <himazin19970630@gmail.com>
2018-09-27 17:20:06 +0900

    関数複数追加

--- Ticket Project/Assets/Scripts/Human/HumanScript.cs
+++ Ticket Project/Assets/Scripts/Human/HumanScript.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 人が持っている情報
+/// </summary>
+public class HumanInfo
+{
+    // チケットの種類
+    private TicketType ticket { get; set; }
+
+    // 通過目標時間
+    private float targetTime { get; set; }
+}
+
 public class HumanScript : MonoBehaviour
 {
-
-    // 不満度
-    private float frustration;
-
+    private Queue<HumanInfo> humanInfos = new Queue<HumanInfo>();
     // Use this for initialization
     void Start()
     {
@@ -20,7 +29,31 @@ public class HumanScript : MonoBehaviour
 
     }
 
-    public void ActionComplate()
+    /// <summary>
+    /// 人生成
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="time"></param>
+    public void CreateHuman()
+    {
+        HumanInfo info = new HumanInfo();
+        
+    }
+
+    /// <summary>
+    /// チケットを入れた時の処理
+    /// </summary>
+    public void TicketIn()
+    {
+
+    }
+
+    /// <summary>
+    /// 不満度の加減処理
+    /// </summary>
+    /// <param name="finishTime"></param>
+    /// <param name="check"></param>
+    public void ActionComplate(float finishTime,bool check)
     {
 
     }
