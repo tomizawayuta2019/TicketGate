7b9dd389dee4fd63d9627f17b1321e2159f86d72
mikuHorie <17d3170023hm@edu.tech.ac.jp>
2018-09-27 15:08:46 +0900

    test

---
+++ Ticket Project/Assets/Scripts/TicketGate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicketGate : MonoBehaviour {
+
+    private enum GateContoroller
+    {
+        //人が来たらチケット情報取得
+        Start,
+        //↑ボタン押されるまで待機
+        Pasumo_OK,
+        //←ボタン押されるまで待機
+        Ticket,
+        //↓ボタン押されるまで待機
+        Ticket_OK1,
+        //→ボタン押されるまで待機
+        Ticket_OK2,
+        //スペース以外の受付NG
+        NO
+    }
+
+    private GateContoroller _gate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (_gate == GateContoroller.Ticket)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+	}
+}
