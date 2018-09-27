71202e555246d74a2a62064f1f01d2b330ce45b2
富澤勇太 <tomizawayuta2019@gmail.com>
2018-09-28 04:06:51 +0900

    SEとエフェクトのマネージャー改修

--- Ticket Project/Assets/Scripts/EffectManager.cs
+++ Ticket Project/Assets/Scripts/EffectManager.cs
@@ -15,6 +15,24 @@ public class EffectManager : SingletonMonoBehaviour<EffectManager> {
     public GameObject PlayEffect(EffectType value,Vector3 position) {
         GameObject effect = Instantiate(effectPrefabs[(int)value]);
         effect.transform.position = position;
+        effect.AddComponent<EffectDest>();
         return effect;
     }
+}
+
+public class EffectDest : MonoBehaviour {
+    Animation animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animation>();
+    }
+
+    private void Update()
+    {
+        if (animator && !animator.isPlaying) {
+            Destroy(gameObject);
+        }
+    }
+
 }
