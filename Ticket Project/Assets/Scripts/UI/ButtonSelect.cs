5d1e83a2b45a0415ffd0379150d7fe7fd6df8be6
富澤勇太 <tomizawayuta2019@gmail.com>
2018-09-29 11:44:53 +0900

    ボタン画像変更

---
+++ Ticket Project/Assets/Scripts/UI/ButtonSelect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSelect : MonoBehaviour {
+    [SerializeField]
+    private List<Button> buttons;
+    private int currentNumber;
+
+    private void Start()
+    {
+        SelectButton(buttons[0]);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter)) {
+            buttons[currentNumber].onClick.Invoke();
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            RemoveButton(buttons[currentNumber]);
+            currentNumber = (currentNumber - 1 + buttons.Count) % buttons.Count;
+            SelectButton(buttons[currentNumber]);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            RemoveButton(buttons[currentNumber]);
+            currentNumber = (currentNumber + 1 + buttons.Count) % buttons.Count;
+            SelectButton(buttons[currentNumber]);
+        }
+    }
+
+    private void SelectButton(Button target) {
+        RectTransform rect = target.GetComponent<RectTransform>();
+        rect.localScale = rect.localScale * 1.1f;
+    }
+
+    private void RemoveButton(Button target) {
+        RectTransform rect = target.GetComponent<RectTransform>();
+        rect.localScale = rect.localScale / 1.1f;
+    }
+}
